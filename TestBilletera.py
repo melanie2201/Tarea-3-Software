e9575ccd6bbdef292818efa6b924538fbb518811
melanie2201 <melanie220196@gmail.com>
2017-02-01 22:35:52 -0400

    Agregando pruebas

--- TestBilletera.py
+++ TestBilletera.py
@@ -16,6 +16,35 @@ class TestBilletera(unittest.TestCase):
     def testIdentificador(self):
         self.b= BilleteraElectronica(1,"Melanie","Gomes",23893494,2212)
         self.assertEqual(self.b.identificador, 1)
+        
+    def testNombre(self):
+        self.b= BilleteraElectronica(1,"Melanie","Gomes",23893494,2212)
+        self.assertEqual(self.b.nombre, "Melanie")
+        
+    def testApellido(self):
+        self.b= BilleteraElectronica(1,"Melanie","Gomes",23893494,2212)
+        self.assertEqual(self.b.apellido, "Gomes")
+        
+    def testCedula(self):
+        self.b= BilleteraElectronica(1,"Melanie","Gomes",23893494,2212)
+        self.assertEqual(self.b.cedula,23893494)
+        
+    def testPin(self):
+        self.b= BilleteraElectronica(1,"Melanie","Gomes",23893494,2212)
+        self.assertEqual(self.b.pin, 2212)
+        
+    def testInicializacion(self):
+        self.b= BilleteraElectronica(1,"Melanie","Gomes",23893494,2212)
+        self.assertEqual(self.b.disp, 0)
+        
+    def testConsumo(self):
+        self.b= BilleteraElectronica(1,"Melanie","Gomes",23893494,2212)
+        self.assertEqual(self.b.consumos.__len__(),0)
+        
+    def testRecargas(self):
+        self.b= BilleteraElectronica(1,"Melanie","Gomes",23893494,2212)
+        self.assertEqual(self.b.recargas.__len__(),0)
+        
     
         
         
