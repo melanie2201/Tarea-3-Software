b47d79c609a60f1854b44f80e3ab92fddffdc64a
melanie2201 <melanie220196@gmail.com>
2017-02-02 09:03:29 -0400

    Casos de prueba malicia

--- TestBilletera.py
+++ TestBilletera.py
@@ -47,26 +47,55 @@ class TestBilletera(unittest.TestCase):
         
     def testFrontera(self):
         self.b = BilleteraElectronica(1,'vero', 'mazu', 1231523, 9929)
-        self.b.recargar(500.00,"enero", 21332)
-        self.b.consumir(500.00 , "enero", 2541,9929)
+        self.b.recargar(float(sys.maxint),"enero", 21332)
+        self.b.consumir(float(sys.maxint), "enero", 2541,9929)
         self.assertEqual(self.b.disp,0)
     
     def testFrontera1(self):
         self.b = BilleteraElectronica(1,'vero', 'mazu', 1231523, 9929)
-        self.b.recargar(500.00,"enero", 2133)
+        self.b.recargar(float(sys.maxint),"enero", 2133)
         self.b.consumir(0.00 , "enero", 2541,9929)
-        self.assertEqual(self.b.disp,500)
+        self.assertEqual(self.b.disp,float(sys.maxint))
 
     def testEsquina(self):
         self.b = BilleteraElectronica(1,'vero', 'mazu', 1231523, 9929)
-        self.b.recargar(500.00,"enero", 21332)
-        self.b.consumir(499.00 , "enero", 2541,9929)
+        self.b.recargar(600.00,"enero", 21332)
+        self.b.consumir(599.00 , "enero", 2541,9929)
         self.assertEqual(self.b.disp,1)
     
     def testEsquina1(self):
         self.b = BilleteraElectronica(1,'vero', 'mazu', 1231523, 9929)
-        self.b.recargar(500.00,"enero", 21332)
+        self.b.recargar(600.00,"enero", 21332)
         self.b.consumir(1.00 , "enero", 2541,9929)
-        self.assertEqual(self.b.disp,499)
+        self.assertEqual(self.b.disp,599)
         
+    def testMalicia1(self):
+        self.b = BilleteraElectronica(1,'vero', 'mazu', 1231523, 9929)
+        self.b.recargar(300.00,"enero", 21332)
+        #Esto debera lanzar una excepcion
+        print("Prueba de malicia 1: ")
+        self.b.consumir(550.30 , "enero", 2541,9929)
+        print("------------------")
+        
+    def testMalicia2(self):
+        self.b = BilleteraElectronica(1,'vero', 'mazu', 1231523, 9929)
+        self.b.recargar(300.00,"enero", 21332)
+        #Esto debera lanzar una excepcion
+        print("Prueba de malicia 2: ")
+        self.b.consumir(250.0 , "enero", 2541,9989)
+        print("------------------")
+        
+    def testMalicia3(self):
+        print("Prueba de malicia 3: ")
+        self.b = BilleteraElectronica(1,'vero', 'mazu', 1231523, 9929)
+        self.b.recargar(-300.00,"enero", 21332)
+        #Esto debera lanzar una excepcion    
+        print("------------------")
+        
+    def testMalicia(self):
+        
+        self.b = BilleteraElectronica(1,'vero', 'mazu', 1231523, 9929)
+        self.b.recargar(300000.00,"enero", 21332)
+        self.b.consumir(10000.00, "enero", 2541,9929)
+        self.assertEqual(self.b.disp,290000)
         
